72fa14e83fc17db9ab64e1f879c3020eb00a88f8
Suvan Sinha <71833649+suvansinha@users.noreply.github.com>
2020-10-19 12:28:54 +0530

    Add files via upload

---
+++ speech recognition.py
@@ -0,0 +1,17 @@
+import speech_recognition as sr
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print('Enter anything: ')
+    audio = r.listen(source)
+    
+    try:  
+         text = r.recognize_google(audio)
+         print('You said : {}'.format(text))
+    except:
+        print('Sorry could not recognize your voice')
+    
+       
+    
+        
